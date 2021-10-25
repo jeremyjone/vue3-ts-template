3487f19c2f014229ff56663cd4a04b97c0aaa77b
jeremyjone <jeremyjone@qq.com>
2021-10-25 15:11:06 +0800

    fix(merge): 添加 vuex 对 ts 的支持

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
-import store from './store/index';
+import store, { key } from './store/index';
 
-createApp(App).use(router).use(store).mount('#app');
+createApp(App).use(router).use(store, key).mount('#app');
