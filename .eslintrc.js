5d8af617d5c7ff0df6f82f80de63bc140d752d35
jeremyjone <jeremyjone@qq.com>
2021-12-20 14:56:19 +0800

    fix(style): 修改 vue 样式格式库版本

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/vue3-essential',
     'plugin:jest/recommended',
     'airbnb-base',
     'airbnb-typescript/base',
